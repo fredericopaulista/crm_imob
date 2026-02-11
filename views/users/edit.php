ad8117902382d3eb7549f7e5c259eafb454949e4
fredericopaulista <fredericopaulista@gmail.com>
2026-02-11 16:38:51 -0300

    fix: Corrigir rotas do formulário de edição de usuários

--- views/users/edit.php
+++ views/users/edit.php
@@ -5,7 +5,8 @@
             <p class="mt-1 text-sm leading-6 text-gray-600">Atualize as informações do usuário.</p>
         </div>
 
-        <form action="<?php echo APP_URL; ?>/user/update/<?php echo $user['id']; ?>" method="POST" class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+        <form action="<?php echo APP_URL; ?>/painel/usuarios/atualizar" method="POST" class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+            <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
             <div class="px-4 py-6 sm:p-8">
                 <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     
@@ -45,7 +46,7 @@
                 </div>
             </div>
             <div class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-                <a href="<?php echo APP_URL; ?>/user" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</a>
+                <a href="<?php echo APP_URL; ?>/painel/usuarios" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</a>
                 <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Salvar Alterações</button>
             </div>
         </form>
