f5abc212b1d4ff815106c5c08ac8236669737a83
fredericopaulista <fredericopaulista@gmail.com>
2026-02-12 09:16:03 -0300

    chore: Ativar logs de debug no SiteController

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -33,11 +33,16 @@ class SiteController {
         $propertyModel = new Property();
         
         if ($slug) {
+            // DEBUG
+            echo "Searching for slug: " . htmlspecialchars($slug) . "<br>";
             $property = $propertyModel->findBySlug($slug);
+            var_dump($property);
         } elseif ($id) {
             $property = $propertyModel->find($id);
         } else {
-            header('Location: ' . APP_URL . '/imoveis');
+            // DEBUG
+            echo "No slug or ID provided<br>";
+             // header('Location: ' . APP_URL . '/imoveis');
             exit;
         }
 
