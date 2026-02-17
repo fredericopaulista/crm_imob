894ab57e83eeac86231ef5a068ed0fa166722575
fredericopaulista <fredericopaulista@gmail.com>
2026-02-17 19:18:09 -0300

    feat: Add cache control headers to disable caching.

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: strict-origin-when-cross-origin");
 header("Content-Security-Policy: default-src 'self' https:; script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com https://cdnjs.cloudflare.com https://ui-avatars.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data: https://ui-avatars.com; connect-src 'self' https://graph.facebook.com;");
 
+// Cache Control
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+
 // Autoload helper
 spl_autoload_register(function ($class_name) {
     if (file_exists('controllers/' . $class_name . '.php')) {
